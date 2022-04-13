a9a728d3b1c979ee47557fa2b51fb54534038e35
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-13 18:25:53 -0400

    Imported the images folder from the course

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src="/images/logos/logo.svg" alt="logo" />
+      <h1>Design and code React apps</h1>
+      <p>
+        Don’t skip design. Learn design and code by building real apps with
+        React and Swift. Complete courses about the best tools.
+      </p>
+    </>
+  );
+}
+
+export default App;
