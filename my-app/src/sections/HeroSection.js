4b988d14ea113fa59cb9aca4ce26e7bb61fb28c8
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-13 23:30:24 -0400

    Imported the mockup animations into the app

--- my-app/src/sections/HeroSection.js
+++ my-app/src/sections/HeroSection.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { H1, MediumText } from "../components/styles/TextStyles";
 import { themes } from "../components/styles/ColorStyles";
 import PurchaseButton from "../components/buttons/PurchaseButton";
+import MockupAnimation from "../components/animations/MockupAnimation";
 
 function HeroSection() {
   return (
@@ -22,6 +23,7 @@ function HeroSection() {
             subtitle="120+ hours of video"
           />
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   );
@@ -31,12 +33,15 @@ export default HeroSection;
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `;
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   padding: 200px 30px;
   margin: 0 auto;
+  display: grid;
+  grid-template-columns: 360px auto;
 `;
 
 const TextWrapper = styled.div`
