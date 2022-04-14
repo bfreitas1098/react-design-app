c7d341c14dc2a0f440e89fbd22c2b15144a9e16b
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-13 20:16:46 -0400

    reset the css and imported all the basic styling and the site logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { default as logo } from "./images/logos/logo.svg";
 
 function App() {
   return (
     <>
-      <img src="/images/logos/logo.svg" alt="logo" />
+      <img src={logo} alt="logo" />
       <h1>Design and code React apps</h1>
       <p>
         Don’t skip design. Learn design and code by building real apps with
