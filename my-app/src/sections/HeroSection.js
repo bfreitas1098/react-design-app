ee340343cf550e0d2420fb912d6ae903ff443ae8
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-13 20:25:48 -0400

    Styled the HeroSection.js file

--- my-app/src/sections/HeroSection.js
+++ my-app/src/sections/HeroSection.js
@@ -1,17 +1,49 @@
 import React from "react";
-import { default as logo } from "../images/logos/logo.svg";
+import styled from "styled-components";
 
 function HeroSection() {
   return (
-    <>
-      <img src={logo} alt="logo" />
-      <h1>Design and code React apps</h1>
-      <p>
-        Don’t skip design. Learn design and code by building real apps with
-        React and Swift. Complete courses about the best tools.
-      </p>
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>
+            Design <br />
+            and code React apps
+          </Title>
+          <Description>
+            Don’t skip design. Learn design and code by building real apps with
+            React and Swift. Complete courses about the best tools.
+          </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   );
 }
 
 export default HeroSection;
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  padding: 200px 30px;
+  margin: 0 auto;
+`;
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`;
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`;
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`;
