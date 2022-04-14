d9e81ec9121ed55bade233fd093ddea65b44dd99
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-04-14 00:14:36 -0400

    Moved the items in the header menu to a separate component

--- my-app/src/components/layout/Header.js
+++ my-app/src/components/layout/Header.js
@@ -2,19 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import { default as logo } from "../../images/logos/logo.svg";
 import { menuData } from "../../data/MenuData";
+import MenuButton from "../buttons/MenuButton";
 
 export default function Header() {
   return (
     <Wrapper>
-      <img src={logo} alt="logo" />
-      <MenuWrapper>
+      <Link href="/">
+        <img src={logo} alt="logo" />
+      </Link>
+      <MenuWrapper count={menuData.length}>
         {menuData.map((item, index) => (
-          <Link href={item.link} key={index}>
-            <MenuItem>
-              <img src={item.icon} alt={item.title} />
-              {item.title}
-            </MenuItem>
-          </Link>
+          <MenuButton key={index} item={item} />
         ))}
       </MenuWrapper>
     </Wrapper>
@@ -34,25 +32,8 @@ const Wrapper = styled.div`
 
 const MenuWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, auto);
+  grid-template-columns: repeat(${(props) => props.count}, auto);
   gap: 30px;
-`;
-
-const MenuItem = styled.div`
-  color: rgba(255, 255, 255, 0.7);
-  display: grid;
-  grid-template-columns: 24px auto;
-  gap: 10px;
-  align-items: center;
-  padding: 10px;
-  border-radius: 10px;
-  transition: 0.5s ease-out;
-
-  :hover {
-    background: rgba(255, 255, 255, 0.1);
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
-      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
-  }
 `;
 
 const Link = styled.a``;
